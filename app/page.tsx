80f0a48f0b44529ab71210e2cb509ff8eb6077b3
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-04-09 19:43:35 +0300

    newest

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "@/app/components/Hero";
+import Newest from "./components/Newest";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Newest />
     </div>
   );
 }
